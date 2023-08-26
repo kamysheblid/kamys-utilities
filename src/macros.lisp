4cd5f6b2e12f117db2186690dcbd594728a21f63
kamysheblid <kamysheblid@gmail.com>
2023-08-27 01:10:54 +0330

    added a with-env macro and corresponding readme info

--- src/macros.lisp
+++ src/macros.lisp
@@ -3,7 +3,7 @@
   (:use :cl)
   (:import-from :alexandria-1 :flatten)
   (:import-from :kamys-utilities/lists #:insert-to-nth)
-  (:export #:-> #:-< #:defmacro/g!))
+  (:export #:-> #:-< #:defmacro/g! #:with-env))
 
 (in-package :kamys-utilities/macros)
 
@@ -83,3 +83,78 @@ _EXAMPLE_
 				      2))))
 	      syms)
 	 ,@body))))
+
+(defmacro with-env (new-env &body body)
+  "Lexical enviornment variables. Use it just like a let form.
+
+Lets you run code with temporarily set environment variables. For
+example: dexedor is an http client, it checks the environment to see
+if HTTPS_PROXY or HTTP_PROXY is set, if it is then it will use the
+proxy. If it isnt then it will continue as normal.
+
+It works using a 4 step process. Steps:
+1. Store old values in env-variable class
+2. Set new vals using loop
+3. Run code body
+4. Set vars back to old vals or unset if old val was nil
+
+NOTE: due to using sb-posix, this only works for sbcl
+
+_EXAMPLE_
+
+> (with-env ((\"VAR\" \"VAL\")
+             (\"PATH\" \"\")) 
+            (format t \"VAR: ~A~%\" (uiop:getenv \"VAR\"))
+            (format t \"PATH: ~A~%\" (uiop:getenv \"PATH\")))
+VAR: VAL
+PATH: 
+NIL
+
+> (format t \"VAR: ~A~%\" (uiop:getenv \"VAR\"))
+VAR: NIL
+NIL
+> (format t \"PATH: ~A~%\" (uiop:getenv \"PATH\"))
+PATH: /usr/bin
+NIL"
+  (let ((env '()))
+    ;; Step 1: store old values in ENV var
+    (loop for (var-name new-val) in new-env
+	  do (push (make-env-variable var-name 
+				      (uiop:getenv var-name)
+				      new-val)
+		   env))
+    ;; Step 2: set new vals
+    (loop for var in env
+	  do (set-env-variable-new-val var))
+    ;; Step 3: run body with env vars
+    (mapcar #'eval body)
+    ;; Step 4: set vars back to old or unset if nil
+    (loop for var in env
+	  do (set-env-variable-old-val var))))
+
+(defclass env-variable ()
+  ((name :initarg :name :accessor get-env-var-name)
+   (old-value :initarg :old-value :accessor get-env-old-value)
+   (new-value :initarg :new-value :accessor get-env-new-value)))
+
+(defun make-env-variable (name old-val new-val)
+  (make-instance 'env-variable
+		 :name name
+		 :old-value old-val
+		 :new-value new-val))
+
+(defun set-env-variable-new-val (env-var)
+  (let ((env-var-name (get-env-var-name env-var))
+	(env-new-value (get-env-new-value env-var)))
+    (setf (uiop:getenv env-var-name) env-new-value)))
+
+(defun set-env-variable-old-val (env-var)
+  (let* ((env-var-name (get-env-var-name env-var))
+	 (env-old-val (get-env-old-value env-var)))
+    (if env-old-val
+	(setf (uiop:getenv env-var-name)
+	      env-old-val)
+	#+sbcl
+	(sb-posix:unsetenv env-var-name)
+	#-sbcl
+	(setf (uiop:getenv env-var-name) env-old-val))))
